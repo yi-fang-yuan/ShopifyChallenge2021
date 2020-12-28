497f7171eebcfc57d69672f4b4f29efa3dd2b424
Yifang Yuan <yi.f.yuan@mail.mcgill.ca>
2020-12-28 17:52:47 -0500

    delete

--- views/src/App.js
+++ views/src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Upload from "./component/Upload";
-import ImageCarousel from "./component/ImageCarousel";
 import ImageBoard from "./component/ImageBoard";
 function App() {
   return (
     <div className="container">
-    {/* <ImageCarousel /> */}
     <Upload />
     <ImageBoard />
     </div>
